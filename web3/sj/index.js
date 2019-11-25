41a6cd8014c0e2fb40bed7149dcfb1e6b3255566
kimjisun317 <jisun3294@naver.com>
2019-11-25 13:18:40 +0900

    1

--- web3/sj/index.js
+++ web3/sj/index.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 	$("#header").load("cni/header.html");
 	$("#footer").load("cni/footer.html");
+	$("#portfolio").load("cni/portfolio.html", function() {
+		tab('#tab',0);
+	}); 
 	
- });
+ });   
+ function tab(e, num){
+	var num = num || 0;
+	var menu = $(e).children();
+	var con = $(e+'_con').children();
+	var select = $(menu).eq(num);
+	var i = num;
+	select.addClass('on');
+	con.eq(num).show();
+	menu.click(function(){
+	   if(select!==null){
+		   select.removeClass("on");
+		   con.eq(i).hide();
+	   }
+	   select = $(this); 
+	   i = $(this).index();
+	   select.addClass('on');
+	   con.eq(i).show();
+	});
+}
+
+
+
+
