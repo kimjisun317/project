c00fd0e2b7b18b951e153eb14eda26dcea030222
kimjisun317 <jisun3294@naver.com>
2019-11-26 13:29:44 +0900

    1

--- web3/sj/index.js
+++ web3/sj/index.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
 	$("#footer").load("cni/footer.html");
 	$("#portfolio").load("cni/portfolio.html", function() {
 		tab('#tab',0);
+		more();
 	}); 
 	
  });   
+
+ //포트폴리오_탭메뉴
  function tab(e, num){
 	var num = num || 0;
 	var menu = $(e).children();
@@ -26,6 +29,50 @@ $(document).ready(function(){
 	});
 }
 
+//포트폴리오_loadmore
+function more() {
+	$('.btn-default').on('click', function (e) {
+		e.preventDefault();
+		var obj = $(this);
+  
+		obj.addClass('active');
+  
+		setTimeout(function () {
+		   obj.removeClass('active');
+		}, 1500);
+  
+		// Load Boats
+		if (obj.hasClass('btn-load-boats')) {
+		   if (!$('.load-boats-box').hasClass('open')) {
+			  $('.load-boats-box').slideToggle(1000);
+			  setTimeout (function () {
+				 $('.load-boats-box').addClass('open');
+			  }, 700);
+		   }
+		}
+  
+		// Load Boats
+		if (obj.hasClass('btn-load-destination')) {
+		   if (!$('.load-destinations-box').hasClass('open')) {
+			  $('.load-destinations-box').slideToggle(1000);
+			  setTimeout (function () {
+				 $('.load-destinations-box').addClass('open');
+			  }, 700);
+		   }
+		}
+	 });
+
+	 $(document).ready(function () {
+		setTimeout(function () {
+		   $('body').addClass('dom-ready');
+		}, 300);
+	 });
+}
 
 
+
+//펼침 레이어
+function layer_View(status){
+	eval('$("#layer_View_box").' + status + '();');
+}
 
