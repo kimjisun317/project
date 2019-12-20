207dc10f19bc80708f140b5fab2d57de23705551
kimjisun317 <jisun3294@naver.com>
2019-12-21 01:51:19 +0900

    1

--- web2/sj/index.js
+++ web2/sj/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$("#header").load("cni/header.html", function () {
+		//헤더캘린더
 		$('.calendar_bxslider').bxSlider({
 			mode: 'horizontal',
 			speed : 1000,
@@ -10,17 +11,17 @@ $(document).ready(function(){
 		  });
 	});
 	$("#footer").load("cni/footer.html", function () {
-		//관련사이트	
-	$('.footer_bottom a').click(function () {
-		$(this).toggleClass('openbox');
-		//$(this).next("ul").toggleClass('openbox');
-		$(this).next("p").slideToggle(300);
-	});
+		//푸터관련사이트	
+		$('.footer_bottom a').click(function () {
+			$(this).toggleClass('openbox');
+			//$(this).next("ul").toggleClass('openbox');
+			$(this).next("p").slideToggle(300);
+		});
 	});
 	
  });
 
-/* main_slider */
+//메인팝업
 $(function(){
 	$('.bxslider').bxSlider({
 	  mode: 'horizontal',
@@ -31,12 +32,10 @@ $(function(){
 	});
 });
 
-
-
-/* layer_campaign */
+//캠페인레이어
 function layer_View(status){
 	eval('$("#layer_View_box").' + status + '();');
-	/* campaign_slider */
+	/* 캠페인슬라이더 */
 	var swiper = new Swiper('.campaign_swiper', { 
 	slidesPerView: 1,
 	spaceBetween: 0,
@@ -53,22 +52,40 @@ function layer_View(status){
 	});
 };
 
-/* media_slider */
+//상영일정슬라이더
 $(function(){
-var swiper = new Swiper('.media_swiper', { 
+	var swiper = new Swiper('.schedule_swiper', { 
+	slidesPerView: 3,	
+	slidesPerGroup: 3,
+	/* spaceBetween: 0, */
+	direction: "horizontal",
+	/* loop : true, */
+	autoplay: false,
+	navigation: {nextEl: '.schedule-button-next', prevEl: '.schedule-button-prev'},
+	roundLengths: true,
+	// 반응형 >= 1250
+	breakpointsInverse: true, 
+	breakpoints: {1041: {slidesPerView: 11, slidesPerGroup: 11}, 
+				  767: {slidesPerView: 7, slidesPerGroup: 7}}
+	});
+});
+
+//미디어슬라이더
+$(function(){
+	var swiper = new Swiper('.media_swiper', { 
 	slidesPerView: 1,
 	spaceBetween: 0,
 	slidesPerGroup: 1,
 	direction: "horizontal",
 	loop : true,
-	autoplay: false, /* {delay: 2500, disableOnInteraction: false, } */
-	/* pagination: {el: '.media-pagination', clickable: true}, */
+	autoplay: false,
 	pagination: {el: '.swiper-pagination', type: 'fraction',},
 	navigation: {nextEl: '.media-button-next', prevEl: '.media-button-prev'},
 	roundLengths: true,
+	// 반응형 >= 1250
 	breakpointsInverse: true, 
 	breakpoints: {1250: {slidesPerView: 4, spaceBetween: 30, slidesPerGroup: 4}, 1024: {slidesPerView: 3, spaceBetween: 30, slidesPerGroup: 3}, 640: {slidesPerView: 2, spaceBetween: 30, slidesPerGroup: 2}}
-});
+	});
 });
 
 
